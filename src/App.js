4564b173c8b925fbaad905e3d0b8115d38ee5e57
eleanorsusanto <145551296+eleanorsusanto@users.noreply.github.com>
2023-11-12 21:40:03 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import "./styles.css";
+import React,{ useState } from "react";
 
-export default function ListingAd({ pic, title, address, description }) {
+var phoneNumberSwitch=false;
+
+
+
+export default function ListingAd({ pic, title, address,project_type, year, ownership_type, 
+  availabilities_label, psf_min, psf_max, subprice_label, description,phoneNumber,num}) {
+
+  const [open,setOpen]=useState(false);
+
   return (
     <div className="App">
-      <img className="mainPic" width="300" height="500" src={pic} />
+      <img className="mainPic" width="500" height="500" src={pic} />
+      <img className="hovers" src="./hover.svg" />
       <div className="mainContent">
-        <h1>{title}</h1>
-        <p className="address">{address}</p>
-        <button>See description</button>
-        <p className="description">{description}</p>
+        <div className="Section">
+          <div className="leftSection">
+            <div className="frame">
+              <img className="icon" src="./building-icon.svg"/>
+              <div className="frameText">
+                <h1>{title}</h1>
+                <p className="address">{address}</p>
+              </div>
+            </div>
+            <div className="descText">
+              <p className="Fill">{project_type} · {year} · {ownership_type}</p>
+              <p className="Fill2">{availabilities_label}</p>
+            </div>         
+          </div>
+          <div className="rightSection">
+            <p className="psf">${psf_min} - ${psf_max} psf</p>
+            <p className="prices">{subprice_label}</p>
+          </div>
+        </div> 
+        
+        <button className="btn" onClick={()=>setOpen(!open)}>See description</button>
+        <div className="desc">
+          {
+            open?
+            <p className="description">{description}
+            </p>
+            :null
+          }
+
+        </div>
+      
+
       </div>
     </div>
   );
