dda9930d4ecefc2dcd6e61a0e0007dd8026fcde8
eleanorsusanto <145551296+eleanorsusanto@users.noreply.github.com>
2023-11-12 21:43:39 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import React,{ useState } from "react";
 
 import App from "./App";
 
+
 const rootElement = document.getElementById("root");
+
+const num ='82345678';
+const phonenumber = num.slice(0,4).padEnd(8, 'X');
+const N ="\n";
+
 const store = {
-  pic: "https://images.prismic.io/99-content/dc1594fb-f413-44ff-b8b5-c3ba6cd539cd_vbWkNuxt2hjgidVttoMRDX.jpg?auto=compress,format&w=2000&q=100",
+  pic: "https://images.prismic.io/99-content/dc1594fb-f413-44ff-b8b5-c3ba6cd539cd_vbWkNuxt2hjgidVttoMRDX.jpg?auto=compress,format&w=1000&q=70",
   title: "Forett At Bukit Timah",
   address: "70 Anchorvale Crescent · D21",
   project_type: "Condo",
@@ -15,13 +22,17 @@ const store = {
   psf_max: 3043,
   subprice_label: "Prices from $1.99M",
   availabilities_label: "231 units · 1, 2, 3, 4 bedrooms",
-  description: `Phone number: 8234 5678
+  description: 
+  `
+  Phone Number: ${phonenumber} ${N}
+  Superb 3 bedroom in sentosa for a very affordable price and a 
+  very nearby bus station.${N}
+  Please call Alex B (CEA No: R0123456) @ ${num} 
+  or the owner ${num} to arrange for a viewing.`,
+  
+}
+;
 
-Superb 3 bedroom in sentosa for a very affordable price and a very nearby bus
-station.
-
-Please call Alex B (CEA No: R0123456) @  82345678 or the owner (83456789) to arrange for a viewing.`,
-};
 
 ReactDOM.render(
   <StrictMode>
